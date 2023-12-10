33c6c70932fc50eacf175c2c4b11c61be78d2ac8
Theorhd <theo.richard8654@gmail.com>
2023-12-10 18:04:08 +0100

    Ajout Resposive Mobile

--- app/public/wp-content/plugins/astra-pro-sites/inc/lib/onboarding/assets/dist/main.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-media-utils', 'wp-url'), 'version' => '4d0193e139dd078d9717');
