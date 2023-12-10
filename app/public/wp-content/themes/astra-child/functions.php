bd8ca0d9b17697f6e2189caba659e2f277ed35f0
Theorhd <theo.richard8654@gmail.com>
2023-12-10 15:57:13 +0100

    Ajout hook "Lien Admin"

--- app/public/wp-content/themes/astra-child/functions.php
+++ app/public/wp-content/themes/astra-child/functions.php
@@ -23,3 +23,18 @@ function child_enqueue_styles() {
 }
 
 add_action( 'wp_enqueue_scripts', 'child_enqueue_styles', 15 );
+
+function ajouter_lien_admin_menu($items, $args) {
+    if (is_user_logged_in() && $args->theme_location == 'primary') {
+        $admin_link = '<li><a class="AdminMenuLien" href="' . admin_url() . '">Admin</a></li>';
+        // Diviser les éléments du menu en un tableau
+        $menu_items = explode('</li>', $items);
+        // Insérer le lien vers l'administration en seconde position
+        array_splice($menu_items, 1, 0, $admin_link);
+        // Réassembler les éléments du menu en une chaîne
+        $items = implode('</li>', $menu_items);
+        $items .= '</li>'; // Ajouter la balise fermante pour le dernier élément
+    }
+    return $items;
+}
+add_filter('wp_nav_menu_items', 'ajouter_lien_admin_menu', 10, 2);
